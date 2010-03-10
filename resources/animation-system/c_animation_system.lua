595bcd2e2817a1c0fe2642b972b1cc44a9ffe5e5
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-10 02:09:29 +0000

    Moar fixes
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@31 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/animation-system/c_animation_system.lua
+++ resources/animation-system/c_animation_system.lua
@@ -3,9 +3,6 @@ local localPlayer = getLocalPlayer()
 local walkanims = { WALK_armed = true, WALK_civi = true, WALK_csaw = true, Walk_DoorPartial = true, WALK_drunk = true, WALK_fat = true, WALK_fatold = true, WALK_gang1 = true, WALK_gang2 = true, WALK_old = true, WALK_player = true, WALK_rocket = true, WALK_shuffle = true, Walk_Wuzi = true, woman_run = true, WOMAN_runbusy = true, WOMAN_runfatold = true, woman_runpanic = true, WOMAN_runsexy = true, WOMAN_walkbusy = true, WOMAN_walkfatold = true, WOMAN_walknorm = true, WOMAN_walkold = true, WOMAN_walkpro = true, WOMAN_walksexy = true, WOMAN_walkshop = true, run_1armed = true, run_armed = true, run_civi = true, run_csaw = true, run_fat = true, run_fatold = true, run_gang1 = true, run_old = true, run_player = true, run_rocket = true, Run_Wuzi = true }
 local attachedRotation = false
 
-bindKey(source, "space", "down", stopAnimation)
-function stopAnimat
-
 function onRender()
 	local forcedanimation = getElementData(localPlayer, "forcedanimation")
 
