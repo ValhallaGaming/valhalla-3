e4cc3c2a83fe2f6aea16f7c69974cd069ba0d7b0
Mount <Mount@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-14 14:07:55 +0000

    /facepalm for last commit
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@58 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/admin-system/Player/s_player_commands.lua
+++ resources/admin-system/Player/s_player_commands.lua
@@ -92,7 +92,7 @@ addCommandHandler("aunmask", adminUnmask, false, false)
 function infoDisplay(thePlayer)
 	if (exports.global:isPlayerAdmin(thePlayer)) then
 		outputChatBox("---[        Useful Information        ]---", getRootElement(), 255, 194, 15)
-		outputChatBox("---[ Ventrilo: server.mta.vg Port: 22003", getRootElement(), 255, 194, 15)
+		outputChatBox("---[ Server: server.mta.vg Port: 22003", getRootElement(), 255, 194, 15)
 		outputChatBox("---[ Ventrilo: 72.37.247.172 Port: 3797", getRootElement(), 255, 194, 15)
 		outputChatBox("---[ Forums: www.ValhallaGaming.net/forums", getRootElement(), 255, 194, 15)
 		outputChatBox("---[ IRC: irc.multitheftauto.com #Valhalla", getRootElement(), 255, 194, 15)
