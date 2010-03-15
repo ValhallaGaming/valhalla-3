d3def371a866a77a607a05a477afe8c3e0b3f00e
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-15 03:18:37 +0000

    Blood effects
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@76 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/fiddycal/c_fiddycal.lua
+++ resources/fiddycal/c_fiddycal.lua
@@ -1,12 +1,18 @@
 function firedFiddyCal(weapon, ammo, clipammo, x, y, z, element)
-	if ( weapon == 34 ) then -- sniper
+	if ( weapon == 34) then -- sniper
 		local px, py, pz = getElementPosition(source)
 		
 		local sound = playSound3D("sound.wav", px, py, pz)
 		setSoundMaxDistance(sound, 150)
 		
-		if ( source == getLocalPlayer() and ( getElementType(element) == "vehicle" or getElementType(element) == "player" ) ) then -- we are the shooter
+		if ( element and source == getLocalPlayer() and ( getElementType(element) == "vehicle" or getElementType(element) == "player" ) ) then -- we are the shooter
 			triggerServerEvent("50cal", getLocalPlayer(), element)
+		end
+		
+		if ( element and getElementType(element) == "player" ) then -- make blood
+			fxAddBlood(x, y, z, 0, 0, 1, 100, 1)
+			fxAddBlood(x, y, z, 1, 0, 0, 100, 1)
+			fxAddBlood(x, y, z, 0, 1, 1, 100, 1)
 		end
 	end
 end
