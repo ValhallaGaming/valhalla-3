f9f504b997675f95baeb09afccf2b9771dcdede1
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-15 00:29:05 +0000

    fix 2
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@72 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/fiddycal/s_fiddycal.lua
+++ resources/fiddycal/s_fiddycal.lua
@@ -1,8 +1,11 @@
 function fiddycal(target)
 	if ( getElementType(target) == "vehicle" ) then -- its a 50cal, it destroys cars!
+		blowVehicle(target, true)
+	elseif ( getElementType(target) == "player" ) then
 		setElementHealth(target, 0)
-	elseif ( getElementType(target) == "vehicle" ) then
-		setElementHealth(target, 0)
+		if ( isElement(target) ) then
+			outputChatBox("You got killed by the 50cal Sniper Rifle!", target, 255, 0, 0)
+		end
 	end
 end
 addEvent("50cal", true)
