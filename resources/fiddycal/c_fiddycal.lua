92879debcb932b7b4512d3438f434e28953c0acf
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-15 00:16:18 +0000

    test 2
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@66 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/fiddycal/c_fiddycal.lua
+++ resources/fiddycal/c_fiddycal.lua
@@ -1,10 +1,12 @@
 function firedFiddyCal(weapon, ammo, clipammo, x, y, z, element)
-	local px, py, pz = getElementPosition(source)
-	
-	playSound3D("sound.wav", px, py, pz)
-	
-	if ( source == getLocalPlayer() and ( getElementType(element) == "vehicle" or getElementType(element) == "player" ) ) then -- we are the shooter
-		triggerServerEvent("50cal", getLocalPlayer(), element)
+	if ( weapon == 34 ) then -- sniper
+		local px, py, pz = getElementPosition(source)
+		
+		playSound3D("sound.wav", px, py, pz)
+		
+		if ( source == getLocalPlayer() and ( getElementType(element) == "vehicle" or getElementType(element) == "player" ) ) then -- we are the shooter
+			triggerServerEvent("50cal", getLocalPlayer(), element)
+		end
 	end
 end
 addEventHandler("onClientPlayerWeaponFire", getRootElement(), firedFiddyCal)
