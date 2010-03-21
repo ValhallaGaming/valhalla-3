d9dbabe69514fa6c929e48b03ad40ec24219e096
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-21 02:11:50 +0000

    Fixed weapons jam bug
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@172 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/realism-system/c_weapon_jams.lua
+++ resources/realism-system/c_weapon_jams.lua
@@ -11,15 +11,16 @@ function playerFired(weapon, ammo, ammoInClip, x, y, z, element)
 			state = 0
 			jammed = true
 			
-			if (pJam) then
+			if (isElement(pJam)) then
 				destroyElement(pJam)
+				pJam = nil
 			end
 			
 			pJam = guiCreateProgressBar(0.425, 0.75, 0.2, 0.035, true)
 			outputChatBox("Your weapon has jammed, Tap - and = in order to unjam your weapons.", 255, 0, 0)
 			local slot = getPedWeaponSlot(getLocalPlayer())
 			triggerServerEvent("togglefiring", getLocalPlayer(), false, true)
-			bindKey("-", "down", unjamWeapon)
+			bindKey("num_sub", "down", unjamWeapon)
 			triggerServerEvent("jammed", getLocalPlayer())
 		end
 	end
@@ -28,12 +29,12 @@ addEventHandler("onClientPlayerWeaponFire", getLocalPlayer(), playerFired)
 
 function unjamWeapon()
 	if (state==0) then
-		bindKey("=", "down", unjamWeapon)
-		unbindKey("-", "down", unjamWeapon)
+		bindKey("num_add", "down", unjamWeapon)
+		unbindKey("num_sub", "down", unjamWeapon)
 		state = 1
 	elseif (state==1) then
-		bindKey("-", "down", unjamWeapon)
-		unbindKey("=", "down", unjamWeapon)
+		bindKey("num_sub", "down", unjamWeapon)
+		unbindKey("num_add", "down", unjamWeapon)
 		state = 0
 	end
 	
@@ -44,15 +45,15 @@ function unjamWeapon()
 		jammed = false
 		triggerServerEvent("notjammed", getLocalPlayer())
 		destroyElement(pJam)
+		pJam = nil
 		triggerServerEvent("togglefiring", getLocalPlayer(), true)
 		outputChatBox("Your weapon is now unjammed.", 0, 255, 0)
-		unbindKey("-", "down", unjamWeapon)
-		unbindKey("=", "down", unjamWeapon)
+		unbindKey("num_sub", "down", unjamWeapon)
+		unbindKey("num_add", "down", unjamWeapon)
 	end
 end
 
 function weaponChangedJammed(prev, curr)
-
 	if (curr==5) then
 		if (jammed) then
 			triggerServerEvent("jammed", getLocalPlayer())
@@ -60,7 +61,7 @@ function weaponChangedJammed(prev, curr)
 			outputChatBox("Your weapon has jammed, Tap - and = in order to unjam your weapons.", 255, 0, 0)
 			local slot = getPedWeaponSlot(getLocalPlayer())
 			triggerServerEvent("togglefiring", getLocalPlayer(), false)
-			bindKey("-", "down", unjamWeapon)
+			bindKey("num_sub", "down", unjamWeapon)
 		end
 	elseif	(prev==5) then
 		jamcount = 0
@@ -72,8 +73,8 @@ function weaponChangedJammed(prev, curr)
 		
 		triggerServerEvent("togglefiring", getLocalPlayer(), true)
 		triggerServerEvent("notjammed", getLocalPlayer())
-		unbindKey("-", "down", unjamWeapon)
-		unbindKey("=", "down", unjamWeapon)
+		unbindKey("num_sub", "down", unjamWeapon)
+		unbindKey("num_add", "down", unjamWeapon)
 	end
 end
 addEventHandler("onClientPlayerWeaponSwitch", getLocalPlayer(), weaponChangedJammed)
