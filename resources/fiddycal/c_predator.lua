04e46b3b74c4600fda12bf4d284ec0f3d1f2b186
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-26 01:19:35 +0000

    test
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@198 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

---
+++ resources/fiddycal/c_predator.lua
@@ -0,0 +1,63 @@
+local localPlayer = getLocalPlayer()
+local projectile = nil
+local projectiles = { }
+local x, y, z = nil
+
+function gimmepredator()
+	projectile = createProjectile(localPlayer, 20, 1153.6865234375, -1391.8251953125, 500.0, 1.0, nil, 90, 0, 0, 0, 0, -1)
+	
+	for i = 1, 10 do
+		projectiles[i] = createProjectile(localPlayer, 20, 1153.6865234375, -1391.8251953125, 100.0, 1.0, nil, 90, 0, 0, 0, 0, -1)
+	end
+	showCursor(false)
+	addEventHandler("onClientPreRender", getRootElement(), updatePredator)
+	addEventHandler("onClientExplosion", localPlayer, explodePredator)
+	addEventHandler("onClientCursorMove", getRootElement(), cursorMove)
+end
+addCommandHandler("pred", gimmepredator)
+
+function cursorMove(cx, cy, absx, absy, wx, wy, wz)
+	if ( isElement(projectile) ) then
+		setElementPosition(projectile, wx, wy, z)
+		
+		for i = 1, 10 do
+			setElementPosition(projectiles[i], wx, wy, z)
+		end
+	end
+end
+
+function updatePredator()
+	if ( isElement(projectile ) ) then
+		setElementVelocity(projectile, 0, 0, -1)
+		local tx, ty, tz = getElementPosition(projectile)
+		
+		if ( tx > 0 ) then
+			x = tx
+			y = ty
+			z = tz
+		end
+	end
+	setCameraMatrix(x, y, z+20, x, y, z)
+	
+	-- heat vision on elements
+	for key, value in ipairs(getElementsByType("vehicle")) do
+		if ( isElementStreamedIn(value) ) then
+			if ( isElementOnScreen(value) ) then
+				local minx, miny, minz, mx, my, mz = getElementBoundingBox(value)
+				local x, y, z = getElementPosition(value)
+				dxDrawLine3D(minx+x, miny+y, mz+z+5, minx+x, my+y ,mz+z+5, tocolor(255, 0, 0, 255), 15, false, 0)
+				dxDrawLine3D(minx+x, miny+y, mz+z+5, mx+x, miny+y ,mz+z+5, tocolor(255, 0, 0, 255), 15, false, 0)
+				dxDrawLine3D(mx+x, my+y, mz+z+5, minx+x, my+y ,mz+z+5, tocolor(255, 0, 0, 255), 15, false, 0)
+				dxDrawLine3D(mx+x, miny+y, mz+z+5, mx+x, my+y ,mz+z+5, tocolor(255, 0, 0, 255), 15, false, 0)
+			end
+		end
+	end
+end
+
+function explodePredator(x, y, z, type)
+	setCameraMatrix(x, y, z+10, x, y, z)
+	setTimer(setCameraTarget, 2500, 1, localPlayer)
+	removeEventHandler("onClientPreRender", getRootElement(), updatePredator)
+	removeEventHandler("onClientExplosion", localPlayer, explodePredator)
+	removeEventHandler("onClientCursorMove", getRootElement(), cursorMove)
+end
