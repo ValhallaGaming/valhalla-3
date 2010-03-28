0e0b94613cdca2019d22dc7f06f4514d9254ac19
Mount <Mount@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-03-28 16:13:07 +0000

    added coroutines in object-system
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@236 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/object-system/s_object_management.lua
+++ resources/object-system/s_object_management.lua
@@ -1,5 +1,6 @@
 mysql = exports.mysql
-
+local threads = {}
+local toLoad = {}
 objects = { }
 objdimension = { }
 local tablec = 1
@@ -23,7 +24,7 @@ function loadDimension(theDimension)
 			objdimension[id] = nil
 		end
 	end
-
+	coroutine.yield()
 	local result = mysql:query(query)
 	if (result) then
 		while true do
@@ -38,7 +39,7 @@ function loadDimension(theDimension)
 				objects[dimension] = { }
 			end
 			
-
+			coroutine.yield()
 			local temparr = { }
 			temparr[1] = tonumber(row["model"])
 			temparr[2] = tonumber(row["posX"])
@@ -55,7 +56,7 @@ function loadDimension(theDimension)
 		end
 		mysql:free_result(result)
 	end
-
+	coroutine.yield()
 	syncDimension(theDimension)
 	return count
 end
@@ -65,7 +66,6 @@ function reloadDimension(thePlayer, commandName, dimensionID)
 		if dimensionID then
 			if (tonumber(dimensionID) >= 0) then
 				triggerClientEvent("object:clear", getRootElement(), dimensionID)
-				
 				local count = loadDimension(tonumber(dimensionID))
 				if (count > 0) then
 					outputChatBox( "Reloaded " .. count .. " items in interior ".. dimensionID, thePlayer, 0, 255, 0 )
@@ -92,6 +92,25 @@ function removeInteriorObjects(theDimension)
 end
 
 function startObjectSystem(res)
-	loadDimension(-1)
+	local result = mysql:query("SELECT `dimension` FROM `objects` ORDER BY `dimension` ASC")
+	if (result) then
+	while true do
+		local row = mysql:fetch_assoc(result)
+		if not row then break end
+		toLoad[tonumber(row["dimension"])] = true
+	end
+	mysql:free_result(result)
+		
+	for id in pairs( toLoad ) do
+		local co = coroutine.create(loadDimension)
+		coroutine.resume(co, id, true)
+		table.insert(threads, co)
+	end
 end
 addEventHandler("onResourceStart", getResourceRootElement(), startObjectSystem)
+
+function resume()
+	for key, value in ipairs(threads) do
+		coroutine.resume(value)
+	end
+end
