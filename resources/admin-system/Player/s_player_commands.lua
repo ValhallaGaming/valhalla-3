ea28536f7ce6afbb22b53d80df2a8f7af891e7f6
Fenix <Fenix@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-04-08 11:31:05 +0000

    Fixed fixwardrobe lol
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@339 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

--- resources/admin-system/Player/s_player_commands.lua
+++ resources/admin-system/Player/s_player_commands.lua
@@ -14,7 +14,7 @@ function fixWardrobe(thePlayer)
 		local dimension = getElementDimension(thePlayer)
 		local interior = getElementInterior(thePlayer)
 		
-		if ( dimension == 65025 and interior == 14 ) then
+		if ( interior == 14 ) then
 			setElementPosition(thePlayer, 1520.0029296875, -1701.2421875, 13.546875)
 			setPedRotation(thePlayer, 275.82971191406)
 			setElementDimension(thePlayer, 0)
