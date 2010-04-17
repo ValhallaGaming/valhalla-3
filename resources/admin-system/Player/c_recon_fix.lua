5d6c7f2d18a600928ccc9036dcb5f6b3cd07f464
mabako <mabako@7ee45a2f-f4cb-2d41-b450-7a63440ea9a9>
2010-04-17 00:57:14 +0000

    fix for recon messing up dumb interiors
    git-svn-id: https://svr2.mount.cc:8443/svn/MTARP/trunk/server@411 7ee45a2f-f4cb-2d41-b450-7a63440ea9a9

---
+++ resources/admin-system/Player/c_recon_fix.lua
@@ -0,0 +1,25 @@
+local timer = false
+local localPlayer = getLocalPlayer( )
+
+local function requestFix( )
+	triggerServerEvent( "fixRecon", localPlayer, getElementAttachedTo( localPlayer ) )
+	timer = nil
+end
+
+addEventHandler( "onClientRender",
+	function( )
+		local element = getElementAttachedTo( localPlayer )
+		if element then
+			if getElementDimension( element ) ~= getElementDimension( localPlayer ) or getElementInterior( element ) ~= getElementInterior( localPlayer ) then
+				if not timer then
+					timer = setTimer( requestFix, 1000, 1 )
+					return
+				end
+			end
+		end
+		if timer then
+			killTimer( timer )
+			timer = nil
+		end
+	end
+)
